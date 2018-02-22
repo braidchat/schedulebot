ea69d6a319fa8c883ce020ce7082bcf52513ebee
James N. V. Cash <james.nvc@gmail.com>
2018-02-21 23:16:57 -0500

    WIP: ability to formulate date ranges exclusions

--- schedule.pl
+++ schedule.pl
@@ -1,9 +1,33 @@
 :- module(schedule, [viable_time/2]).
 :- use_module(library(clpfd)).
-:- use_module(library(julian)).
+:- use_module(library(delay), [delay/1]).
+:- use_module(library(julian), [form_time/2, datetime/3]).
+:- use_module(library(julian/util), [dow_number/2]).
+:- use_module(library(list_util), [xfy_list/3]).
+
+:- multifile julian:form_time/2.
+julian:form_time(not(dow(Day)), Dt) :-
+    ground(Day),
+    datetime(Dt, MJD, _),
+    dow_number(Day, DayNumber),
+    (MJD+2) mod 7 #\= DayNumber.
+julian:form_time(not(Times), Dt) :-
+    form_time(Times, NotDt),
+    datetime(Dt, MJD, Ns),
+    datetime(NotDt, NotMJD, NotNs),
+    fd_dom(NotMJD, NotMJDDom),
+    fd_dom(NotNs, NotNSDom),
+    MJD in NotMJDDom #\ Ns in NotNSDom.
+
+julian:form_time(day_hours(D, Hs), Dt) :-
+    form_time([D, H:_:_], Dt),
+    xfy_list(\/, Domain, Hs),
+    H in Domains.
 
 viable_time(Constraints, Dt) :-
     form_time(Constraints, Dt),
+    form_time(H:_:_, Dt),
+    H in 9..18,
     julian:datetime(Dt, MJD, Nanos),
     labeling([leftmost,up,bisect], [MJD]),
     labeling([leftmost,up,bisect], [Nanos]).
@@ -15,7 +39,20 @@ rfc_time(D, S) :-
     form_time(rfc3339(RfcCodes), D),
     string_codes(S, RfcCodes).
 
-?- all_viable_times([after(2018-02-21), before(2018-02-28), _H:00:00], Ds),
+print_list([]).
+print_list([A|As]) :-
+    format('~w~n', [A]), print_list(As).
+
+?- H in 10..13,
+   all_viable_times([after(2018-02-21), before(2018-02-24), H:00:00,
+                     %% day_hours(true, [11])
+                     %% not(day_hours(2018-02-22, [10, 12])),
+                     %% not(dow(friday)),
+                     true
+                    ],
+                    Ds),
    maplist(schedule:rfc_time, Ds, Rfcs),
-   [D1, D2, D3|_] = Rfcs,
-   format('first times ~w, ~w, ~w ~n', [D1, D2, D3]).
+   format('~n', []),
+   print_list(Rfcs).
+
+%% ?- A in 0..3, B in 1..4, clpfd:all_distinct([A, B]).
